9c4511968ca958a52505aa0a59dd0713ce98489f
MasterPluzh <136272134+MasterPluzh@users.noreply.github.com>
2023-06-12 21:45:25 -0400

    Update App_Comuna.py

--- App_Comuna.py
+++ App_Comuna.py
@@ -5,6 +5,33 @@ import tkinter as tk
 ventana = tk.Tk()
 ventana.iconbitmap("icono.ico")
 
+# Clase Boton Para crear un Boton
+
+class Colocar_Boton:
+    def __init__(self,ventana,texto,color_fondo,color_letra,comando=None):
+        boton = tk.Button(
+            ventana
+            ,text=texto
+            ,bg=color_fondo
+            ,fg=color_letra
+            ,font=("Arial",16)
+            ,width=10
+            ,height=2
+            ,relief="raised"
+            ,bd=5
+            ,wraplength=100
+            ,command=comando
+            )
+        boton.pack(
+            side=tk.LEFT
+            ,padx=5
+            ,pady=5
+            ,ipadx=5
+            ,ipady=5
+            ,fill=tk.NONE
+            ,expand=False
+            )
+
 # Funciones
 
 def ventana_menu():
@@ -14,222 +41,102 @@ def ventana_menu():
     ventana.title("Menu de Incio")
     ventana.geometry("250x300+550+100")
 
-    boton_opcion_consejo_comunal = tk.Button(
-        ventana
-        ,text="Consejos Comunales"
-        ,bg="yellow"
-        ,fg="red"
-        ,font=("Arial", 16)
-        ,width=10
-        ,height=2
-        ,relief="raised"
-        ,bd=5,wraplength=100
-        ,command=lambda: ventana_control_opciones(1)
-        )
-    boton_opcion_consejo_comunal.pack(
-        side=tk.TOP
-        ,padx=5
-        ,pady=5
-        ,ipadx=5
-        ,ipady=5
-        ,fill=tk.NONE
-        ,expand=False
+    frame = tk.Frame(ventana), frame.pack(side=tk.TOP)
+
+    boton_opcion_consejo_comunal = Colocar_Boton(
+        frame
+        ,"Consejos Comunales"
+        ,"yellow"
+        ,"red"
+        ,lambda: ventana_control_opciones(1)
         )
 
-    boton_opcion_voceros = tk.Button(
-        ventana
-        ,text="Voceros"
-        ,bg="blue"
-        ,fg="yellow"
-        ,font=("Arial", 16)
-        ,width=10,height=2
-        ,relief="raised"
-        ,bd=5
-        ,wraplength=100
-        ,command=lambda: ventana_control_opciones(2)
-        )
-    boton_opcion_voceros.pack(
-        side=tk.TOP
-        ,padx=5
-        ,pady=5
-        ,ipadx=5
-        ,ipady=5
-        ,fill=tk.NONE
-        ,expand=False
+    frame = tk.Frame(ventana), frame.pack(side=tk.TOP)
+
+    boton_opcion_vocero = Colocar_Boton(
+        frame
+        ,"Voceros"
+        ,"blue"
+        ,"yellow"
+        ,lambda: ventana_control_opciones(1)
         )
 
-    boton_salir = tk.Button(
-        ventana
-        ,text="Salir"
-        ,bg="red"
-        ,fg="yellow"
-        ,font=("Arial", 16)
-        ,width=10
-        ,height=2
-        ,relief="raised"
-        ,bd=5
-        ,wraplength=100
-        ,command=ventana.quit
-        )
-    boton_salir.pack(
-        side=tk.TOP
-        ,padx=5
-        ,pady=5
-        ,ipadx=5
-        ,ipady=5
-        ,fill=tk.NONE
-        ,expand=False
+    frame = tk.Frame(ventana), frame.pack(side=tk.TOP)
+
+    boton_salir = Colocar_Boton(
+        frame
+        ,"Salir"
+        ,"red"
+        ,"blue"
+        ,ventana.quit
         )
 
 def ventana_control_opciones(consejo_or_vocero):
     for widget in ventana.winfo_children():
         widget.destroy()
     
-    if consejo_or_vocero == 1: ventana.title("Control de Consejos Comunales")
-    elif consejo_or_vocero == 2: ventana.title("Control de Voceros")
+    if consejo_or_vocero == 1:
+        ventana.title("Control de Consejos Comunales")
+    
+    elif consejo_or_vocero == 2:
+        ventana.title("Control de Voceros")
 
     # ventana.title("Control de Voceros")
     ventana.geometry("400x300+475+100")
 
-    frame_1 = tk.Frame(ventana)
-    frame_1.pack(side=tk.TOP)
+    frame_1 = tk.Frame(ventana),frame_1.pack(side=tk.TOP)
 
-    boton_opcion_agregar = tk.Button(
-        frame_1,
-        text="Agregar"
-        ,bg="white"
-        ,fg="black"
-        ,font=("Arial",16)
-        ,width=10
-        ,height=2
-        ,relief="raised"
-        ,bd=5
-        ,wraplength=100
-        )
-    boton_opcion_agregar.pack(
-        side=tk.LEFT
-        ,padx=5
-        ,pady=5
-        ,ipadx=5
-        ,ipady=5
-        ,fill=tk.NONE
-        ,expand=False
+    boton_opcion_agregar = Colocar_Boton(
+        frame_1
+        ,"Agregar"
+        ,"white"
+        ,"black"
+        ,None
         )
 
-    boton_opcion_quitar = tk.Button(
-        frame_1,text="Quitar"
-        ,bg="white"
-        ,fg="black"
-        ,font=("Arial",16)
-        ,width=10
-        ,height=2
-        ,relief="raised"
-        ,bd=5
-        ,wraplength=100
-        )
-    boton_opcion_quitar.pack(
-        side=tk.LEFT
-        ,padx=5
-        ,pady=5
-        ,ipadx=5
-        ,ipady=5
-        ,fill=tk.NONE
-        ,expand=False
+    boton_opcion_quitar = Colocar_Boton(
+        frame_1
+        ,"Quitar"
+        ,"white"
+        ,"black"
+        ,None
         )
 
     frame_2 = tk.Frame(ventana)
     frame_2.pack(side=tk.TOP)
 
-    boton_opcion_editar = tk.Button(
+    boton_opcion_editar = Colocar_Boton(
         frame_2
-        ,text="Editar"
-        ,bg="white"
-        ,fg="black"
-        ,font=("Arial",16)
-        ,width=10,
-        height=2,
-        relief="raised"
-        ,bd=5
-        ,wraplength=100
-        )
-    boton_opcion_editar.pack(
-        side=tk.LEFT
-        ,padx=5
-        ,pady=5
-        ,ipadx=5
-        ,ipady=5
-        ,fill=tk.NONE
-        ,expand=False
+        ,"Editar"
+        ,"white"
+        ,"black"
+        ,None
         )
 
-    boton_opcion_consultar = tk.Button(
+    boton_opcion_consultar = Colocar_Boton(
         frame_2
-        ,text="Consultar"
-        ,bg="white"
-        ,fg="black"
-        ,font=("Arial",16)
-        ,width=10
-        ,height=2
-        ,relief="raised"
-        ,bd=5
-        ,wraplength=100
-        )
-    boton_opcion_consultar.pack(
-        side=tk.LEFT
-        ,padx=5
-        ,pady=5
-        ,ipadx=5
-        ,ipady=5
-        ,fill=tk.NONE
-        ,expand=False
+        ,"Consultar"
+        ,"white"
+        ,"black"
+        ,None
         )
 
-    frame_3 = tk.Frame(ventana)
-    frame_3.pack(side=tk.TOP)
+    frame_3 = tk.Frame(ventana),frame_3.pack(side=tk.TOP)
 
-    boton_opcion_mostrar = tk.Button(
+    boton_opcion_mostrar = Colocar_Boton(
         frame_3
-        ,text="Mostrar"
-        ,bg="white"
-        ,fg="black"
-        ,font=("Arial",16)
-        ,width=10
-        ,height=2
-        ,relief="raised"
-        ,bd=5
-        ,wraplength=100
-        )
-    boton_opcion_mostrar.pack(
-        side=tk.LEFT
-        ,padx=5
-        ,pady=5
-        ,ipadx=5
-        ,ipady=5
-        ,fill=tk.NONE
-        ,expand=False
+        ,"Mostrar"
+        ,"white"
+        ,"black"
+        ,None
         )
 
-    boton_volver = tk.Button(
+    boton_opcion_volver = Colocar_Boton(
         frame_3
-        ,text="Volver"
-        ,bg="white"
-        ,fg="black"
-        ,font=("Arial",16)
-        ,width=10
-        ,height=2
-        ,relief="raised"
-        ,bd=5
-        ,wraplength=100
-        ,command=ventana_menu
-        )
-    boton_volver.pack(
-        side=tk.TOP
-        ,padx=5
-        ,pady=5
-        ,ipadx=5
-        ,ipady=5
-        ,fill=tk.NONE
-        ,expand=False
+        ,"Volver"
+        ,"white"
+        ,"black"
+        ,ventana_menu
         )
 
 # Widgets
